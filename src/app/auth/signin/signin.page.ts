4a588ec62847269e9886c0531cc24591c9869ca8
alicarlo <alicarlo.montijop@gmail.com>
2023-11-21 13:16:50 -0800

    add validations forms and add loader

--- src/app/auth/signin/signin.page.ts
+++ src/app/auth/signin/signin.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/services/firebase/auth.service';
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController } from '@ionic/angular';
 import { ToastService } from 'src/app/services/toast.service';
 import { UsersService } from 'src/app/services/firebase/users.service';
 import { map } from 'rxjs/operators';
@@ -16,30 +16,48 @@ export class SigninPage implements OnInit {
 
   loginForm: FormGroup;
   loading = false;
-  
+  error_messages={
+		'email':[
+      {type: 'required', message: 'Correo requerido'},
+      {type: 'minlength', message: 'Minimo 3 caracteres'},
+			{type: 'maxlength', message: 'Maximo 35 caracteres'},
+    ],
+    'password':[
+      {type: 'required', message: 'Contraseña requerida'},
+      {type: 'minlength', message: 'Minimo 4 caracteres'},
+			{type: 'maxlength', message: 'Maximo 35 caracteres'}
+    ],
+  }
   constructor(
     private fb: FormBuilder,
     private authService: AuthService,
     public navController: NavController,
     private toastService: ToastService,
     private usersService: UsersService,
-    private storageService: StorageService
+    private storageService: StorageService,
+		private _LoadingController:LoadingController
   ) {
     this.loginForm = fb.group({
-			email: ['', Validators.compose([Validators.required, Validators.email])],
-			password: ['', Validators.compose([Validators.required, Validators.minLength(8)])]
+			email: ['', Validators.compose([Validators.required, Validators.email, Validators.minLength(3),Validators.maxLength(35)])],
+			password: ['', Validators.compose([Validators.required, Validators.minLength(4), Validators.maxLength(35)])]
 		});
    }
 
   ngOnInit() {
   }
 
-  signin() {
+  async signin() {
     if(this.loginForm.valid) {
+			const loading = await this._LoadingController.create({
+				message: 'Cargando...',
+			});
+			await loading.present();
       this.authService.signin(this.loginForm.value).then((response:any) => {
         // if (response.user) {
           const user = response.user;
           if (!user.emailVerified) {
+						this.toastService.presentToast('Su cuenta requeire ser verificada', 3000, 'warning')
+						loading.dismiss();
             this.navController.navigateForward('auth/verify');
           } else {
             this.usersService.getUser(user.uid).pipe(
@@ -70,7 +88,9 @@ export class SigninPage implements OnInit {
                   })
                 })
               }
+							loading.dismiss();
             },(error) => {
+							loading.dismiss();
 							this.toastService.presentToast(error, 3000, 'danger')
 						})
           }
@@ -78,10 +98,9 @@ export class SigninPage implements OnInit {
 					this.toastService.presentToast('Opss! Error al iniciar sesion', 3000, 'danger')
         }*/
       }).catch( err => {
+				loading.dismiss();
 				this.toastService.presentToast(err, 3000, 'danger')
 			})
     }
   }
-
-
 }
